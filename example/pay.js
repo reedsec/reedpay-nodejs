a335f7f7610d170ed62bd83d8be9347ced2ad6a1
i5hz <i@5hz.org>
2016-10-24 11:25:33 +0800

    update example

--- example/pay.js
+++ example/pay.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const API_KEY = "sk_test_ibbTe5jLGCi5rzfH4OqPW9KC";
+const API_KEY = "1234";
 const APP_ID = "app_57ede786d4d7a81549645e56";
 const reedpay = require('../lib/reedpay')(API_KEY);
 
@@ -22,19 +22,7 @@ switch (channel) {
 
 const now = new Date();
 const order_no = Date.parse(now);
-reedpay.setProtocol('http');
-// console.log(reedpay.getApiField('auth'));
-// console.log(reedpay.getApiField('protocol'));
-// console.log(reedpay.getApiField('host'));
-// console.log(reedpay.getApiField('port'));
-// console.log(reedpay.getApiField('basePath'));
-// console.log(reedpay.getApiField('version'));
-// console.log(reedpay.getApiField('timeout'));
-// console.log(reedpay.getApiField('dev'));
-// console.log(reedpay.getConstant('USER_AGENT'));
-
-// console.log('resource', reedpay.resources);
-
+reedpay.setProtocol('https');
 
 reedpay.payment.create({
   mch_order_no: order_no.toString(),
