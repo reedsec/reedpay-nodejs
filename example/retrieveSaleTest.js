cdc5d44209e8a05abff398bd7e7006b1271efc92
Hengzhi <wuhz@reedsec.com>
2017-04-05 20:14:54 +0800

    update retrieve

---
+++ example/retrieveSaleTest.js
@@ -0,0 +1,26 @@
+'use strict';
+
+// const API_KEY = "sk_test_149532cd63d8624cd6e43f2a13898669";
+// const APP_ID = 'app_585b91e40382f6615980fe54';
+
+
+// const APP_ID = 'app_58de1746fd897549f29ef7ee';
+const API_KEY = 'sk_test_11c3ae948a95c343ab68044f4797c829';
+
+const reedpay = require('../lib/reedpay')(API_KEY);
+
+// reedpay.setPrivateKeyPath(__dirname + '/reedpay_private_key.pem');
+// reedpay.setSecretKey('secret');
+// reedpay.setSecretKey('8.8.8.8');
+// reedpay.setSecretKey('1234');
+reedpay.setHost('127.0.0.1', '80', 'http');
+
+
+reedpay.sale.retrieve(
+  function(err, res) {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(res);
+});
